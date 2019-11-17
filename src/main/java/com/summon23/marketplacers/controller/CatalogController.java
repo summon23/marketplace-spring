c92d0a06d30a442f3abf809c2cd83b267ace223e
summon23 <andreymahdison@gmail.com>
2019-11-18 00:15:07 +0700

    implementing paging on product vendor

--- src/main/java/com/summon23/marketplacers/controller/CatalogController.java
+++ src/main/java/com/summon23/marketplacers/controller/CatalogController.java
@@ -4,16 +4,24 @@ import com.summon23.marketplacers.entity.ProductVariant;
 import com.summon23.marketplacers.entity.ProductVendor;
 import com.summon23.marketplacers.repository.ProductVariantRepository;
 import com.summon23.marketplacers.repository.ProductVendorRepository;
+import com.summon23.marketplacers.service.ProductVendorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("catalog")
 public class CatalogController {
+
+    private final static Logger logger = LoggerFactory.getLogger(CatalogController.class);
 
     @Autowired
     private final ProductVariantRepository productVariantRepository;
@@ -21,9 +29,24 @@ public class CatalogController {
     @Autowired
     private final ProductVendorRepository productVendorRepository;
 
+    @Autowired
+    private ProductVendorService productVendorService;
+
     public CatalogController(ProductVariantRepository productVariantRepository, ProductVendorRepository productVendorRepository) {
         this.productVariantRepository = productVariantRepository;
         this.productVendorRepository = productVendorRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductVendor>> getProductVariant(
+            @RequestParam(defaultValue = "0") Integer pageNo,
+            @RequestParam(defaultValue = "10") Integer pageSize,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortOrder
+    ) {
+        List<ProductVendor> listCatalogVendor = productVendorService.findAllPaginated(pageNo, pageSize, sortBy, sortOrder);
+
+        return new ResponseEntity<List<ProductVendor>>(listCatalogVendor, new HttpHeaders(), HttpStatus.OK);
     }
 
     @Transactional
@@ -33,7 +56,7 @@ public class CatalogController {
             @RequestBody Map<String, String> newCatalog) {
 
         String newLongName = newCatalog.get("long_name");
-        ProductVendor pv =  productVendorRepository.findById(catalogId).orElseThrow(() -> new RuntimeException("NOT FOUND"));
+        ProductVendor pv =  productVendorService.findById(catalogId);
 
         pv.setCurrency(newLongName);
         productVendorRepository.save(pv);
@@ -42,16 +65,14 @@ public class CatalogController {
         pVar.setLongName("NEW SKU");
         productVariantRepository.save(pVar);
 
-        if (newLongName.equals("USD")) throw new RuntimeException("ROLLBACK");
+        if (newLongName.equals("USD")) {
+            logger.info("THROW");
+            throw new RuntimeException("ROLLBACK");
+        }
+
+        logger.info("Success");
 
         return ResponseEntity.ok().build();
     }
 
-//    @PostMapping("/vendor")
-//    public ResponseEntity<?> insertProductVendor() {
-//        ProductVendor newProduct = new ProductVendor( new Long(112233));
-//        productVendorService.insertProductVendor(newProduct);
-//
-//        return ResponseEntity.ok().build();
-//    }
 }
